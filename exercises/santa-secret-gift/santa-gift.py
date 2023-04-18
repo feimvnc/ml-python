5dcbddc1ffe4f2d56b09fb3b4e56363748d276ca
feimvnc <feimvnc@gmail.com>
2023-04-18 22:15:58 +0100

    update santa-gift.py

--- exercises/santa-secret-gift/santa-gift.py
+++ exercises/santa-secret-gift/santa-gift.py
@@ -1,6 +1,7 @@
 #tested at below 
 # https://coderpad.io/resources/docs/for-candidates/interview-preparation-guide/sandbox/i
 
+## method 1
 from typing import List
 def say_hello(l: List) -> List:
     #print('Hello, World')
@@ -17,4 +18,29 @@ def say_hello(l: List) -> List:
 l = list('abcdef')
 #for i in range(5):
 print(f"name list is: {say_hello(l)}")
+
+
+
+## method 2
+import random
+def say_hello():
+    print('Hello, World')
+    a = list("abcdef")
+    b = random.sample(a, k=len(a))
+    b_2 = b[::-1]
+    results = [[i, j] for (i, j) in zip(b, b_2)]
+    for item in results: 
+        #print(item[0], item[1])
+        if item[0] == item[1]:
+             print("error, rule broken")
+             break
+    print(results)
+    #help(random)
+
+for i in range(10):
+    say_hello()
+
+
+
+
 
